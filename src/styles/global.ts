e9adda492b972e3cdbf502d5c495ec5fc0744592
ygor-salles <ygorsallesaniceto@gmail.com>
2022-06-09 22:56:55 -0300

    adjusting global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,57 +1,53 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-      --background: #f0f2f5;
-      --red: #e52e4d;
-      --green: #33cc95;
-      --blue: #5429cc;
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --green: #33cc95;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #ffffff;
+  }
 
-      --blue-light: #6933ff;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-      --text-title: #363f5f;
-      --text-body: #969cb3;
-
-      --background: #f8f2f5;
-      --shape: #ffffff;
-    }
-    
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: 0;
-    }
-
-    html {
-      @media (max-width: 1080px) {
-        font-size: 93.75%;
-      }
-
-      @media (max-width: 720px) {
-        font-size: 87.5%;
-      }
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
     }
 
-    body {
-      background: var(--background);
-      -webkit-font-smoothing: antialiased;
+    @media (max-width: 720px) {
+      font-size: 87.5%;
     }
+  }
 
-    body, input, textarea, button {
-      font-family: 'Poppins' sans-serif;
-      font-weight: 400;
-    }
+  body {
+    background-color: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
 
-    h1, h2, h3, h4, h5, h6, strong {
-      font-weight: 600;
-    }; 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
 
-    button {
-      cursor: pointer;
-    }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 
-    [disabled] {
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
 `;
